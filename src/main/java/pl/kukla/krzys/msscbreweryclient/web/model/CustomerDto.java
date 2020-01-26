123248718840c1b1d155836e35d9357ec076b59c
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-26 21:35:55 +0100

    46 - Client microservice

---
+++ src/main/java/pl/kukla/krzys/msscbreweryclient/web/model/CustomerDto.java
@@ -0,0 +1,21 @@
+package pl.kukla.krzys.msscbreweryclient.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * @author Krzysztof Kukla
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+    private UUID uuid;
+    private String name;
+
+}
---
+++ src/test/java/pl/kukla/krzys/msscbreweryclient/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
